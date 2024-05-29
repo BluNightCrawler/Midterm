668810a0fd7736244e4a830f005382f2f2d7c398
BluNightCrawler <jeremyfrance123@hotmail.com>
2024-05-29 00:59:42 -0700

    THis my midterm project

---
+++ app/src/main/java/com/example/midterm/ViewCourse.java
@@ -0,0 +1,59 @@
+package com.example.midterm;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ViewCourse extends AppCompatActivity {
+    private ArrayList<CourseModel> courseModalArrayList;
+    private DBHandler dbHandler;
+    private CourseRVAdapter courseRVAdapter;
+    private RecyclerView coursesRV;
+    private Button create;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_course);
+        // initializing our all variables.
+        courseModalArrayList = new ArrayList<>();
+        dbHandler = new DBHandler(ViewCourse.this);
+
+        create = findViewById(R.id.idBtnCreate);
+
+        // getting our course array
+        // list from db handler class.
+        courseModalArrayList = dbHandler.readCourses();
+
+        // on below line passing our array list to our adapter class.
+        courseRVAdapter = new CourseRVAdapter(courseModalArrayList, ViewCourse.this);
+        coursesRV = findViewById(R.id.idRVCourses);
+
+        // setting layout manager for our recycler view.
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(ViewCourse.this, RecyclerView.VERTICAL, false);
+        coursesRV.setLayoutManager(linearLayoutManager);
+
+        // setting our adapter to recycler view.
+        coursesRV.setAdapter(courseRVAdapter);
+
+        create.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(ViewCourse.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+    };
+    }
